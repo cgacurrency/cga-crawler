40a3b75b26f57fc187aa06778fbf219deeb5505a
0op9io8ui <50051576+0op9io8ui@users.noreply.github.com>
2019-10-27 00:51:09 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,26 @@
 module.exports = {
   apps: [
-    {
-      name: "explorer-api",
-      script: "server.api.js",
-      instances: 4,
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      }
-    },
-    {
-      name: "explorer-peers",
-      script: "server.peers.js",
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      }
-    },
+    // {
+    //   name: "explorer-api",
+    //   script: "server.api.js",
+    //   instances: 4,
+    //   env: {
+    //     NODE_ENV: "development"
+    //   },
+    //   env_production: {
+    //     NODE_ENV: "production"
+    //   }
+    // },
+    // {
+    //   name: "explorer-peers",
+    //   script: "server.peers.js",
+    //   env: {
+    //     NODE_ENV: "development"
+    //   },
+    //   env_production: {
+    //     NODE_ENV: "production"
+    //   }
+    // },
     {
       name: "explorer-top-accounts",
       script: "server.top-accounts.js",
