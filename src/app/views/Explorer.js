da39a85dfb8aac6516277a79ae4cf144d12df711
BBIBBI <43683301+albertphil@users.noreply.github.com>
2020-07-02 11:19:19 +0900

    update

--- src/app/views/Explorer.js
+++ src/app/views/Explorer.js
@@ -94,7 +94,9 @@ class Explorer extends React.PureComponent {
             <RandomVerifiedAccounts count={5} />
           </div>
 
-
+          <div className="mt-5">
+            <ExplorerTopAccounts count={5} />
+          </div>
 
 
         </div>
