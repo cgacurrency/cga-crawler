28e506f04939e9bde91f0d0006beece6fc74c758
BBIBBI <43683301+albertphil@users.noreply.github.com>
2020-08-10 08:42:43 +0900

    Update util.js

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,7 +3,9 @@ import config from "client-config.json";
 
 export function formatTimestamp(timestamp) {
   if (!timestamp) return null;
+  console.log('timestamp:', moment.unix(parseInt(timestamp, 10)).format("MMM D, YYYY HH:mm:ss"));
   return moment.unix(parseInt(timestamp, 10)).format("MMM D, YYYY HH:mm:ss");
+
 }
 
 export function validateAddress(address) {
